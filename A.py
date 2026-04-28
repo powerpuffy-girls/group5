6f7ee4be3da24d59913f4af49c230eb9e31b2bd0
MOLINA,ANGEL <molina.angelyvonne.lnhs@gmail.com>
2026-04-28 12:52:37 +0800

    Add files via upload

---
+++ A.py
@@ -0,0 +1,13 @@
+FILENAME = "my_diary.py"
+
+def create_file():
+    try:
+        file = open(FILENAME, "x") 
+        file.close()
+        print("Diary file created!\n")
+    except FileExistsError:
+        print("Diary file already exists.\n")
+
+def write_entry():
+    entry = input("Write your diary:\n")
+    
